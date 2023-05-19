4aa9a07f3a123776ba131803983d7b4fc4c0265c
Meashyshy <camillefvrl@gmail.com>
2023-05-19 07:18:56 -0700

    Added (Head & Sub texts) + (Responsiveness to small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,9 +13,17 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
 
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#6AC9A4]">Camille</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              A software engineering student <br className="sm:block hidden" /> aiming to become a full-stack developer
+            </p>
+          </div>
         </div>
-      </div>
+
+        <ComputersCanvas />
     </section>
   )
 }
