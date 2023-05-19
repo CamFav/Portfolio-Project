4dc88bd7e7737b59735049d1ebd0e4d44cd07b93
Meashyshy <camillefvrl@gmail.com>
2023-05-19 02:20:05 -0700

    Fixed navbar - Added white hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,12 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState(' ')
+  const [active, setActive] = useState("");
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between-items-center max-w-7xl mx-auto">
+    <nav className={`${
+      styles.paddindX
+      } w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
           to="/" 
           className="flex items-center gap-2" 
@@ -19,7 +21,21 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Camille&nbsp;<span className="sm:block hidden">| Favriel</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
